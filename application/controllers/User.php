63507d2401ef76fa3a335c3dacb1386160faaf6d
Viter <913294974@qq.com>
2018-07-06 22:28:54 +0800

    update用户开户，充值

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,6 +5,7 @@
  * Date: 2018/7/2
  * Time: 21:35
  */
+//21000
 class UserController extends \Base\ApplicationController
 {
 
@@ -35,6 +36,7 @@ class UserController extends \Base\ApplicationController
         $this->assign('pager', $pager);
         $this->assign('username',$username);
         $this->assign('name',$company);
+        $this->assign('accounts',$this->_accounts);
     }
 
     /**
@@ -75,6 +77,74 @@ class UserController extends \Base\ApplicationController
         $this->assign('time',$time);
         $this->assign('acounts',$this->_accounts);
         $this->assign('actions',$this->_actions);
+    }
+
+
+    /**
+     * 创瑞云开户
+     * @return false
+     */
+    public function insertAction(){
+        $username = $this->getParam('username','','string');
+        $passward = $this->getParam('password','','string');
+        $companyName = $this->getParam('companyName','','string');
+        $type = $this->getParam('type',0,'int');
+        $access_key = $this->getParam('access_key','','string');
+        $secret = $this->getParam('secret','','string');
+        if(empty($username) || empty($passward)|| empty($access_key)|| empty($secret)|| empty($companyName)){
+            return $this->returnData('数据不能为空',21000);
+        }
+        if(strlen($passward)<6){
+            return $this->returnData('密码长度不能小于6位',21002);
+        }
+        if(!$type){
+            return $this->returnData('用户类型错误',21003);
+        }
+        $mapper = \Mapper\UsersModel::getInstance();
+        $model = new \UsersModel();
+        $model->setUsername($username);
+        $model->setAccess_key($access_key);
+        $model->setSecret($secret);
+        $model->setPassword(\Ku\Tool::encryption($passward));
+        $model->setCreated_at(date('Y-m-d H:i:s'));
+        $model->setUpdated_at(date('Y-m-d H:i:s'));
+        $model->setType($type);
+        $model->setName($companyName);
+        $res = $mapper->insert($model);
+        if(!$res){
+            return $this->returnData('开户失败，请重试',21004);
+        }
+        return $this->returnData('开户成功',21001,true);
+    }
+
+
+    /**
+     * 用户充值
+     * @return false
+     */
+    public function rechargeAction(){
+        $userid = $this->getParam('userid',0,'int');
+        $recharge = $this->getParam('recharge',0,'int');
+        if(empty($recharge)){
+            return $this->returnData('请输入正确的充值数目',21006);
+        }
+        $mapper = \Mapper\UsersModel::getInstance();
+        $user = $mapper->findById($userid);
+        if(!$user instanceof \UsersModel){
+            return $this->returnData('充值用户不存在',21007);
+        }
+        $update = array(
+            'normal_balance'=>'normal_balance+'.$recharge,
+            'marketing_balance'=>'marketing_balance+'.$recharge,
+            'show_normal_balance'=>'show_normal_balance+'.$recharge,
+            'show_marketing_balance'=>'show_marketing_balance+'.$recharge,
+            );
+        $where = array('id'=>$userid);
+        $res = $mapper->update($update,$where);
+        if(!$res){
+            return $this->returnData('充值失败，请重试!',21008);
+        }
+        return $this->returnData('充值成功',21009,true);
     }
 
 
